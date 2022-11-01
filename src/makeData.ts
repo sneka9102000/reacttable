a6b37e1784bced3f72999e3fcb7e6422ff070211
sneka9102000 <96578887+sneka9102000@users.noreply.github.com>
2022-11-01 13:07:34 +0530

    Initial commit

---
+++ src/makeData.ts
@@ -0,0 +1,38 @@
+import namor from 'namor';
+
+const range = (len: number) => {
+  const arr = [];
+  for (let i = 0; i < len; i++){
+    arr.push(i);
+  }
+  return arr;
+}
+
+const newPerson = () => {
+  const statusChange = Math.random();
+  return {
+    firstName: namor.generate({ words: 1, numbers: 0}), 
+    lastName: namor.generate({ words: 1, numbers: 0}),
+    age: Math.floor(Math.random() * 30),
+    visits: Math.floor(Math.random() * 100),
+    progress: Math.floor(Math.random() * 100),
+    status:
+      statusChange > 0.66 ?
+        'relationship' : statusChange > 0.33 ?
+        'complicated' : 'single'
+  }
+}
+
+export default function makeData(...lens) {
+  const makeDataLevel = (depth: number = 0) => {
+    const len = lens[depth];
+    return range(len).map(d => {
+      return {
+        ...newPerson(),
+        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
+      }
+    })
+  }
+
+  return makeDataLevel();
+}
